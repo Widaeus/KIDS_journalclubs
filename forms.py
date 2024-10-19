99dbb4ca5b6017a8f8344b670f862a0151b28990
Jacob Widaeus <jacob.widaeus@gmail.com>
2024-10-19 16:49:25 +0200

    added choices to units

--- forms.py
+++ forms.py
@@ -2,6 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, TimeField, SubmitField
 from wtforms.validators import DataRequired, Email
 
+units = [
+    ('Anestesi och Intensivvård', 'Anestesi och Intensivvård'),
+    ('Hjärtmedicin', 'Hjärtmedicin'),
+    ('Infektionssjukdomar', 'Infektionssjukdomar'),
+    ('Kirurgi och urologi', 'Kirurgi och urologi'),
+    ('Medicin', 'Medicin'),
+    ('Medicinsk pedagogik', 'Medicinsk pedagogik'),
+    ('Neurologi', 'Neurologi'),
+    ('Njurmedicin', 'Njurmedicin'),
+    ('Obstetrik och gynekologi', 'Obstetrik och gynekologi'),
+    ('Ortopedi', 'Ortopedi'),
+    ('Rehabiliteringsmedicin', 'Rehabiliteringsmedicin')
+]
+
 class RegisterJournalClubForm(FlaskForm):
     theme = StringField('Tema', validators=[DataRequired()])
     date = DateField('Datum', validators=[DataRequired()])
@@ -12,6 +26,6 @@ class RegisterJournalClubForm(FlaskForm):
 class RegisterStudentForm(FlaskForm):
     first_name = StringField('Förnamn', validators=[DataRequired()])
     last_name = StringField('Efternamn', validators=[DataRequired()])
-    unit = StringField('Enhet', validators=[DataRequired()])
+    unit = StringField('Enhet', choices=units, validators=[DataRequired()])
     ki_email = StringField('KI E-mail', validators=[DataRequired(), Email()])
     submit = SubmitField('Register Student')
