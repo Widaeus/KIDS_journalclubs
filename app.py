ae94d0bea11082a35cce0b7e9f7e6d00440b639f
Jacob Widaeus <jacob.widaeus@gmail.com>
2024-10-21 14:32:54 +0200

    update

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 
 # Set the SECRET_KEY for CSRF protection
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'b_5#y2L"F4Q8z\n\xec]/')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+if not app.config['SECRET_KEY']:
+    raise RuntimeError("SECRET_KEY is not set in the environment variables")
 
 # Use the DATABASE_URL environment variable for PostgreSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://kids_journalclub_x7v0_user:Mxj1Jl1YECnjyIltWQpLvIIfdt59o0VC@dpg-csb3ms2j1k6c73d0tvu0-a.frankfurt-postgres.render.com/kids_journalclub_x7v0')
